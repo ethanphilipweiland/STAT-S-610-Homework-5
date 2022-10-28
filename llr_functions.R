279248b99eb7b0a748bc43baac53d0b142cd5702
ethanphilipweiland <ethanphilipweiland@gmail.com>
2022-10-28 00:33:15 -0400

    Speeding up llr: take 2

--- llr_functions.R
+++ llr_functions.R
@@ -23,7 +23,7 @@ llr <- function(x, y, z, omega) {
 compute_f_hat <- function(z, x, y, omega) {
   Wz <- diag(make_weight_matrix(z, x, omega))
   X <- make_predictor_matrix(x)
-  f_hat = c(1, z) %*% solve(t(X) %*% (Wz * X)) %*% t(X) %*%  (Wz * y)
+  f_hat <- c(1, z) %*% solve(t(X) %*% sweep(x = X, MARGIN = 1, STATS = Wz, FUN = "*")) %*% t(X) %*% (Wz * y)
   return(f_hat)
 }
 
